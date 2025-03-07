96dbf658392bae62d0c0845fe773dc1138f3ac27
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-07 10:43:21 -0800

    added file

--- src/axios/Axios.jsx
+++ src/axios/Axios.jsx
@@ -1,11 +1,37 @@
-
-
+import axios from "axios"
+import { useEffect } from "react"
+import { useState } from "react"
 
 function Axios() {
-    return (
-        <div>
 
-        </div>
+    const [cycle, setCycle] = useState([])
+
+    useEffect(() => {
+
+        axios.get('https://raw.githubusercontent.com/minhazapon/Axios-in-react.js/refs/heads/main/public/axios.json')
+            .then(res => {
+                setCycle(res.data)
+
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+
+    }, [])
+
+
+    return (
+        <>
+
+            <div>
+                <div>
+
+
+
+                </div>
+            </div>
+
+        </>
     )
 }
 
