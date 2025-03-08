97f0ef3d81c7ce60907be2e8de4f1b4e733173f7
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-07 21:55:03 -0800

    added file

--- src/axios/Route.jsx
+++ src/axios/Route.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react"
+import { AllData } from "./AxApi"
+import { useEffect } from "react"
 
 
 function Route() {
 
+    const [dataZ, setData] = useState([])
+
+    const GetPostData = async () => {
+        const res = await AllData()
+        setData(res.data)
+    }
+
+    useEffect(() => {
+        GetPostData()
+    }, [])
 
     return (
         <div>
