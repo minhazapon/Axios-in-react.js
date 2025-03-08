40e4b2c0ba72bee074da83c6f9e20c349e360eca
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-08 00:33:22 -0800

    added file

--- src/axios/Details.jsx
+++ src/axios/Details.jsx
@@ -5,7 +5,7 @@ function Details({ information }) {
 
     return (
         <div>
-            <div className="card w-96 h-[500px] bg-black text-white card-md shadow-sm">
+            <div className="card w-96 h-[550px] bg-black text-white card-md shadow-sm">
                 <div className="">
                     <div>
                         <img className=" " src="https://s44885.pcdn.co/wp-content/uploads/2019/09/Fotolia_257957753_Subscription_Monthly_M.jpg" alt="" />
@@ -14,8 +14,9 @@ function Details({ information }) {
                         <h2 className="card-title">ID: {id} </h2>
                         <h2 className="card-title">Title: {title} </h2>
                         <p className=" mt-6 ">Body: {body} </p>
-                        <div className="justify-end card-actions">
-                            <button className="btn bg-black text-white">Buy Now</button>
+                        <div className=" flex justify-end items-center gap-2 mt-5">
+                            <button className="btn bg-black text-white">EDIT</button>
+                            <button className="btn bg-red-600 text-white">DELETE</button>
                         </div>
                     </div>
                 </div>
