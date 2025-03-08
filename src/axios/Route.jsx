01c98e6ee841d10cd197e76056ebd804e64ea5c2
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-07 22:03:26 -0800

    added file

--- src/axios/Route.jsx
+++ src/axios/Route.jsx
@@ -2,7 +2,6 @@ import { useState } from "react"
 import { AllData } from "./AxApi"
 import { useEffect } from "react"
 
-
 function Route() {
 
     const [dataZ, setData] = useState([])
@@ -20,9 +19,26 @@ function Route() {
         <>
             <div>
                 <div className="m-20">
-
-                    <p> Axios </p>
-
+                    <p className="text-center text-5xl font-bold text-black">Axios Data</p>
+                    <div>
+                        <div>
+                            {
+                                dataZ.map(information => <ul key={information.id}>
+                                    <div>
+                                        <div className="card w-96 bg-black text-white card-md shadow-sm">
+                                            <div className="card-body">
+                                                <h2 className="card-title">{dataZ.title}</h2>
+                                                <p>{dataZ.body}</p>
+                                                <div className="justify-end card-actions">
+                                                    <button className="btn bg-black text-white">Buy Now</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </ul>)
+                            }
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
