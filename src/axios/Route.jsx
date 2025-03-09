ee5a8f0b5f461faacf3e30c8f8d78f6c6acf0975
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-08 20:33:22 -0800

    added file

--- src/axios/Route.jsx
+++ src/axios/Route.jsx
@@ -39,8 +39,18 @@ function Route() {
                     <div className=" flex justify-center ">
                         <div className=" flex justify-center mt-10 border-[1px] p-5 w-fit rounded-xl bg-violet-50 ">
                             <form className=" flex-col md:flex-row lg:flex-row flex items-center gap-3 ">
-                                <input required className=" h-[30px] w-[300px] p-2 border-[1px]" placeholder="Add Title" type="text" />
-                                <input required className=" h-[30px] w-[300px] p-2 border-[1px]" placeholder="Add Body" type="text" />
+                                <input
+                                    required
+                                    className=" h-[30px] w-[300px] p-2 border-[1px]"
+                                    name="title"
+                                    placeholder="Add Title"
+                                    type="text" />
+                                <input
+                                    required
+                                    className="h-[30px] w-[300px] p-2 border-[1px]"
+                                    name="body"
+                                    placeholder="Add Body"
+                                    type="text" />
                                 <input className=" bg-blue-900 text-white h-[30px] w-[300px] cursor-pointer " type="submit" value="Add" />
                             </form>
                         </div>
