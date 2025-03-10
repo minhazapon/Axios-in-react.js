affa7bf599c62716937f24f8e13af6bdf629fcd6
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-10 01:16:36 -0700

    added file

--- src/axios2/Xios.jsx
+++ src/axios2/Xios.jsx
@@ -16,6 +16,7 @@ function Xios() {
         GetAllData();
     }, [])
 
+    //delete section
     const handleDelete = async (id) => {
         try {
 
@@ -31,9 +32,31 @@ function Xios() {
         }
     }
 
+
+
     return (
         <div className="m-10">
             <p className=" text-2xl text-center text-black">All Axios Comments</p>
+            <div className=" flex justify-center ">
+                <div className=" flex justify-center mt-10 border-[1px] p-5 w-fit rounded-xl bg-violet-50 ">
+                    <form className=" flex-col md:flex-row lg:flex-row flex items-center gap-3 ">
+                        <input
+                            required
+                            className=" h-[30px] w-[300px] p-2 border-[1px]"
+                            name="title"
+                            placeholder="Add Title"
+                            type="text" />
+                        <input
+                            required
+                            className="h-[30px] w-[300px] p-2 border-[1px]"
+                            name="body"
+                            placeholder="Add Body"
+                            type="text" />
+                        <input className=" bg-blue-900 text-white h-[30px] w-[300px] cursor-pointer " type="submit" value="Add" />
+                    </form>
+                </div>
+            </div>
+
             <div className=" flex justify-center mt-10 ">
                 <div className=" grid md:grid-cols-3 gap-5 ">
                     {
