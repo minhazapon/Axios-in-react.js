f964a3c9a29f5d12c539185528e05b43e8c14fc8
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-09 23:32:31 -0700

    added file

--- src/axios2/Xdetails.jsx
+++ src/axios2/Xdetails.jsx
@@ -7,10 +7,14 @@ function Xdeatils({ cmntValue }) {
     return (
         <div>
             <div>
-                <div className="card w-96 h-[200px] bg-black text-white card-lg shadow-sm">
+                <div className="card w-96 h-[250px] bg-black text-white card-lg shadow-sm">
                     <div className="card-body">
                         <h2 className="card-title"> {name} </h2>
                         <p> {email} </p>
+                    </div>
+                    <div className=" flex justify-end gap-5 p-4 ">
+                        <button className=" btn bg-green-700 text-white ">EDIT</button>
+                        <button className=" btn bg-red-700 text-white ">DELETE</button>
                     </div>
                 </div>
             </div>
